506e9ae21be2aef9947b21fd2679a1bd16f1aa0c
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-06-13 23:09:30 +0530

    feat: DirectCameraScreen

---
+++ lib/widgets/direct_camera.dart
@@ -0,0 +1,174 @@
+/*
+ * This file is a part of Bluecherry Client (https://https://github.com/bluecherrydvr/bluecherry_client).
+ *
+ * Copyright 2022 Bluecherry, LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+import 'package:bluecherry_client/widgets/device_tile.dart';
+import 'package:bluecherry_client/providers/mobile_view_provider.dart';
+import 'package:bluecherry_client/providers/server_provider.dart';
+import 'package:bluecherry_client/api/api.dart';
+import 'package:status_bar_control/status_bar_control.dart';
+
+class DirectCameraScreen extends StatefulWidget {
+  const DirectCameraScreen({Key? key}) : super(key: key);
+
+  @override
+  State<DirectCameraScreen> createState() => _DirectCameraScreenState();
+}
+
+class _DirectCameraScreenState extends State<DirectCameraScreen> {
+  @override
+  void initState() {
+    SystemChrome.setPreferredOrientations(DeviceOrientation.values);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          splashRadius: 20.0,
+          onPressed: Scaffold.of(context).openDrawer,
+        ),
+        title: Text('direct_camera'.tr()),
+      ),
+      body: ServersProvider.instance.servers.isEmpty
+          ? Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    Icons.dns,
+                    size: 72.0,
+                    color: Theme.of(context).iconTheme.color?.withOpacity(0.8),
+                  ),
+                  const SizedBox(height: 8.0),
+                  Text(
+                    'no_servers_added'.tr(),
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline5
+                        ?.copyWith(fontSize: 16.0),
+                  ),
+                ],
+              ),
+            )
+          : ListView.builder(
+              itemCount: ServersProvider.instance.servers.length,
+              itemBuilder: (context, i) {
+                final server = ServersProvider.instance.servers[i];
+                return FutureBuilder(
+                  future: (() async => server.devices.isEmpty
+                      ? API.instance.getDevices(
+                          await API.instance.checkServerCredentials(server))
+                      : true)(),
+                  builder: (context, snapshot) {
+                    return snapshot.hasData
+                        ? ListView.builder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: server.devices.length + 1,
+                            itemBuilder: (context, index) => index == 0
+                                ? Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                      horizontal: 24.0,
+                                      vertical: 16.0,
+                                    ),
+                                    child: Text(
+                                      server.name.toUpperCase(),
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .overline
+                                          ?.copyWith(
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                    ),
+                                  )
+                                : () {
+                                    index--;
+                                    return ListTile(
+                                      enabled: server.devices[index].status,
+                                      leading: CircleAvatar(
+                                        child: const Icon(Icons.camera_alt),
+                                        backgroundColor: Colors.transparent,
+                                        foregroundColor:
+                                            Theme.of(context).iconTheme.color,
+                                      ),
+                                      title: Text(
+                                        server.devices[index].name
+                                            .split(' ')
+                                            .map((e) =>
+                                                e[0].toUpperCase() +
+                                                e.substring(1))
+                                            .join(' '),
+                                      ),
+                                      subtitle: Text([
+                                        server.devices[index].status
+                                            ? 'online'.tr()
+                                            : 'offline'.tr(),
+                                        server.devices[index].uri,
+                                        '${server.devices[index].resolutionX}x${server.devices[index].resolutionY}',
+                                      ].join(' • ')),
+                                      onTap: () async {
+                                        final player = MobileViewProvider
+                                            .instance
+                                            .getVideoPlayerController(
+                                                server.devices[index]);
+                                        SystemChrome.setPreferredOrientations([
+                                          DeviceOrientation.landscapeLeft,
+                                          DeviceOrientation.landscapeRight,
+                                        ]);
+                                        StatusBarControl.setHidden(true);
+                                        await Navigator.of(context).push(
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                DeviceFullscreenViewer(
+                                              device: server.devices[index],
+                                              ijkPlayer: player,
+                                            ),
+                                          ),
+                                        );
+                                        await player.release();
+                                        await player.release();
+                                        SystemChrome.setPreferredOrientations(
+                                            DeviceOrientation.values);
+                                        StatusBarControl.setHidden(false);
+                                      },
+                                    );
+                                  }(),
+                          )
+                        : Center(
+                            child: Container(
+                              alignment: Alignment.center,
+                              height: 156.0,
+                              child: const CircularProgressIndicator(),
+                            ),
+                          );
+                  },
+                );
+              },
+            ),
+    );
+  }
+}

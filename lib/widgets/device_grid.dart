09b2992b4082bdf62ae06e52efc8f35907b65ce2
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-10-05 13:33:47 -0500

    fix: camera tile labels clipped on newer iOS

--- lib/widgets/device_grid.dart
+++ lib/widgets/device_grid.dart
@@ -17,6 +17,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+import 'dart:io';
 import 'dart:math';
 
 import 'package:animations/animations.dart';
@@ -162,109 +163,106 @@ class _MobileDeviceGridState extends State<MobileDeviceGrid> {
   @override
   Widget build(BuildContext context) {
     return Consumer<MobileViewProvider>(
-      builder: (context, view, _) => SizedBox(
-        child: Column(
-          children: [
-            view.tab == -1
-                ? const Spacer()
-                : Expanded(
-                    child: PageTransitionSwitcher(
-                      child: {
-                        1: () => const _MobileDeviceGridChild(tab: 1),
-                        2: () => const _MobileDeviceGridChild(tab: 2),
-                        4: () => const _MobileDeviceGridChild(tab: 4),
-                      }[view.tab]!(),
-                      transitionBuilder:
-                          (child, primaryAnimation, secondaryAnimation) =>
-                              FadeThroughTransition(
-                        animation: primaryAnimation,
-                        secondaryAnimation: secondaryAnimation,
-                        child: child,
-                        fillColor: Colors.black,
-                      ),
+      builder: (context, view, _) => Column(
+        children: [
+          view.tab == -1
+              ? const Spacer()
+              : Expanded(
+                  child: PageTransitionSwitcher(
+                    child: {
+                      1: () => const _MobileDeviceGridChild(tab: 1),
+                      2: () => const _MobileDeviceGridChild(tab: 2),
+                      4: () => const _MobileDeviceGridChild(tab: 4),
+                    }[view.tab]!(),
+                    transitionBuilder:
+                        (child, primaryAnimation, secondaryAnimation) =>
+                            FadeThroughTransition(
+                      animation: primaryAnimation,
+                      secondaryAnimation: secondaryAnimation,
+                      child: child,
+                      fillColor: Colors.black,
                     ),
                   ),
-            Container(
-              decoration: const BoxDecoration(
-                boxShadow: [
-                  BoxShadow(color: Colors.black45, blurRadius: 8.0),
-                ],
-              ),
-              child: Material(
-                color: Theme.of(context).primaryColor,
-                child: Container(
-                  height: kMobileBottomBarHeight,
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  width: MediaQuery.of(context).size.width,
-                  child: Row(
-                    children: <Widget>[
-                          IconButton(
-                            onPressed: Scaffold.of(context).openDrawer,
-                            icon: const Icon(Icons.menu),
-                            iconSize: 18.0,
-                            color: Colors.white,
-                            splashRadius: 24.0,
-                          ),
-                          const Spacer(),
-                        ] +
-                        [1, 2, 4].map((e) {
-                          final child = view.tab == e
-                              ? Card(
-                                  margin: EdgeInsets.zero,
-                                  elevation: 4.0,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20.0),
-                                  ),
+                ),
+          Container(
+            decoration: const BoxDecoration(
+              boxShadow: [
+                BoxShadow(color: Colors.black45, blurRadius: 8.0),
+              ],
+            ),
+            child: Material(
+              color: Theme.of(context).primaryColor,
+              child: Container(
+                height: kMobileBottomBarHeight,
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  children: <Widget>[
+                        IconButton(
+                          onPressed: Scaffold.of(context).openDrawer,
+                          icon: const Icon(Icons.menu),
+                          iconSize: 18.0,
+                          color: Colors.white,
+                          splashRadius: 24.0,
+                        ),
+                        const Spacer(),
+                      ] +
+                      [1, 2, 4].map((e) {
+                        final child = view.tab == e
+                            ? Card(
+                                margin: EdgeInsets.zero,
+                                elevation: 4.0,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                ),
+                                child: Container(
+                                  height: 40.0,
+                                  width: 40.0,
+                                  alignment: Alignment.center,
                                   child: Container(
-                                    height: 40.0,
-                                    width: 40.0,
+                                    height: 28.0,
+                                    width: 28.0,
                                     alignment: Alignment.center,
-                                    child: Container(
-                                      height: 28.0,
-                                      width: 28.0,
-                                      alignment: Alignment.center,
-                                      child: Text(
-                                        e.toString(),
-                                        style: const TextStyle(
-                                          color: Colors.black,
-                                          fontSize: 18.0,
-                                        ),
-                                      ),
-                                      decoration: BoxDecoration(
-                                        border: Border.all(
-                                          color: Theme.of(context).primaryColor,
-                                          width: 2.0,
-                                        ),
-                                        borderRadius:
-                                            BorderRadius.circular(14.0),
+                                    child: Text(
+                                      e.toString(),
+                                      style: const TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 18.0,
                                       ),
                                     ),
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: Theme.of(context).primaryColor,
+                                        width: 2.0,
+                                      ),
+                                      borderRadius: BorderRadius.circular(14.0),
+                                    ),
                                   ),
+                                ),
+                                color: Colors.white,
+                              )
+                            : Text(
+                                e.toString(),
+                                style: const TextStyle(
                                   color: Colors.white,
-                                )
-                              : Text(
-                                  e.toString(),
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 18.0,
-                                  ),
-                                );
-                          return Container(
-                            height: 48.0,
-                            width: 48.0,
-                            alignment: Alignment.centerRight,
-                            child: IconButton(
-                              onPressed: () => view.setTab(e),
-                              icon: child,
-                            ),
-                          );
-                        }).toList(),
-                  ),
+                                  fontSize: 18.0,
+                                ),
+                              );
+                        return Container(
+                          height: 48.0,
+                          width: 48.0,
+                          alignment: Alignment.centerRight,
+                          child: IconButton(
+                            onPressed: () => view.setTab(e),
+                            icon: child,
+                          ),
+                        );
+                      }).toList(),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -318,17 +316,25 @@ class _MobileDeviceGridChild extends StatelessWidget {
             2: 2,
           }[tab]!,
           childAspectRatio: <int, double>{
-            4: (MediaQuery.of(context).size.width -
-                    MediaQuery.of(context).padding.horizontal) /
-                (MediaQuery.of(context).size.height -
-                    kMobileBottomBarHeight -
-                    MediaQuery.of(context).padding.bottom),
-            2: (MediaQuery.of(context).size.width -
-                    MediaQuery.of(context).padding.horizontal) *
-                0.5 /
-                (MediaQuery.of(context).size.height -
-                    kMobileBottomBarHeight -
-                    MediaQuery.of(context).padding.bottom),
+            if (Platform.isIOS) ...{
+              4: (MediaQuery.of(context).size.width) /
+                  (MediaQuery.of(context).size.height - kMobileBottomBarHeight),
+              2: (MediaQuery.of(context).size.width) *
+                  0.5 /
+                  (MediaQuery.of(context).size.height - kMobileBottomBarHeight),
+            } else ...{
+              4: (MediaQuery.of(context).size.width -
+                      MediaQuery.of(context).padding.horizontal) /
+                  (MediaQuery.of(context).size.height -
+                      kMobileBottomBarHeight -
+                      MediaQuery.of(context).padding.bottom),
+              2: (MediaQuery.of(context).size.width -
+                      MediaQuery.of(context).padding.horizontal) *
+                  0.5 /
+                  (MediaQuery.of(context).size.height -
+                      kMobileBottomBarHeight -
+                      MediaQuery.of(context).padding.bottom),
+            }
           }[tab]!,
           mainAxisSpacing: 0.0,
           crossAxisSpacing: 0.0,

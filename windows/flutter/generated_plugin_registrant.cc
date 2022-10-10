f6ff5bb2fbc78e9334bd37f2fc75e40dcc699de6
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-10-10 13:32:58 -0500

    style: iOS app icon & launch screen

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,21 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <dart_vlc/dart_vlc_plugin.h>
-#include <flutter_native_view/flutter_native_view_plugin.h>
-#include <screen_retriever/screen_retriever_plugin.h>
+#include <permission_handler_windows/permission_handler_windows_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
-#include <window_manager/window_manager_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  DartVlcPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("DartVlcPlugin"));
-  FlutterNativeViewPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FlutterNativeViewPlugin"));
-  ScreenRetrieverPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("ScreenRetrieverPlugin"));
+  PermissionHandlerWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
-  WindowManagerPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("WindowManagerPlugin"));
 }

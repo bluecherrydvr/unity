a885fb931e0bd07bf32f5af19f854d1da5715c96
Bruno D'Luka <brunodlukaa@gmail.com>
2023-04-11 14:04:47 -0300

    Improve server menu
    * Added menu for mobile, which means it's possible to perform actions to the servers as well
    * Added "refreshServer" localization
    * Show menu on secondary tap (right click)

--- lib/widgets/settings/server_tile.dart
+++ lib/widgets/settings/server_tile.dart
@@ -159,41 +159,60 @@ class ServerTile extends StatelessWidget {
     final isLoading = servers.isServerLoading(server);
     final loc = AppLocalizations.of(context);
 
-    return ListTile(
-      leading: CircleAvatar(
-        backgroundColor: Colors.transparent,
-        foregroundColor:
-            server.online ? theme.iconTheme.color : theme.colorScheme.error,
-        child: Icon(server.online ? Icons.dns : Icons.desktop_access_disabled),
-      ),
-      title: Text(
-        server.name,
-        overflow: TextOverflow.ellipsis,
-      ),
-      subtitle: Text(
-        !isLoading
-            ? [
-                if (server.name != server.ip) server.ip,
-                if (server.online)
-                  loc.nDevices(server.devices.length)
-                else
-                  loc.offline,
-              ].join(' • ')
-            : AppLocalizations.of(context).gettingDevices,
-        overflow: TextOverflow.ellipsis,
-      ),
-      trailing: IconButton(
-        icon: Icon(
-          Icons.delete,
-          color: theme.colorScheme.error,
-        ),
-        tooltip: loc.disconnectServer,
-        splashRadius: 24.0,
-        onPressed: () => onRemoveServer(context, server),
-      ),
-      onTap: () {
-        showEditServer(context, server);
+    void showMenu([Offset? position]) {
+      final box = context.findRenderObject() as RenderBox;
+      final pos = position ?? box.localToGlobal(Offset(box.size.width, 0));
+
+      showServerMenu(
+        context: context,
+        onRemoveServer: onRemoveServer,
+        server: server,
+        pos: pos,
+      );
+    }
+
+    return GestureDetector(
+      onSecondaryTapUp: (d) {
+        showMenu(d.globalPosition);
       },
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundColor: Colors.transparent,
+          foregroundColor:
+              server.online ? theme.iconTheme.color : theme.colorScheme.error,
+          child:
+              Icon(server.online ? Icons.dns : Icons.desktop_access_disabled),
+        ),
+        title: Text(
+          server.name,
+          overflow: TextOverflow.ellipsis,
+        ),
+        subtitle: Text(
+          !isLoading
+              ? [
+                  if (server.name != server.ip) server.ip,
+                  if (server.online)
+                    loc.nDevices(server.devices.length)
+                  else
+                    loc.offline,
+                ].join(' • ')
+              : AppLocalizations.of(context).gettingDevices,
+          overflow: TextOverflow.ellipsis,
+        ),
+        trailing: IconButton(
+          icon: Icon(
+            Icons.delete,
+            color: theme.colorScheme.error,
+          ),
+          tooltip: loc.disconnectServer,
+          splashRadius: 24.0,
+          onPressed: () => onRemoveServer(context, server),
+        ),
+        onTap: () {
+          showEditServer(context, server);
+        },
+        onLongPress: showMenu,
+      ),
     );
   }
 }
@@ -211,133 +230,169 @@ class ServerCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final home = context.watch<HomeProvider>();
     final servers = context.watch<ServersProvider>();
 
     final isLoading = servers.isServerLoading(server);
 
     final loc = AppLocalizations.of(context);
 
-    return SizedBox(
-      height: 180,
-      width: 180,
-      child: Card(
-        child: Stack(children: [
-          Positioned.fill(
-            bottom: 8.0,
-            left: 8.0,
-            right: 8.0,
-            top: 8.0,
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              CircleAvatar(
-                backgroundColor: Colors.transparent,
-                foregroundColor: theme.iconTheme.color,
-                child: const Icon(Icons.dns, size: 30.0),
-              ),
-              const SizedBox(height: 8.0),
-              Text(
-                server.name,
-                overflow: TextOverflow.ellipsis,
-                style: theme.textTheme.titleSmall,
-              ),
-              Text(
-                !isLoading
-                    ? [
-                        if (server.name != server.ip) server.ip,
-                      ].join()
-                    : loc.gettingDevices,
-                overflow: TextOverflow.ellipsis,
-                style: theme.textTheme.bodySmall,
-              ),
-              Text(
-                !server.online
-                    ? loc.offline
-                    : !isLoading
-                        ? loc.nDevices(server.devices.length)
-                        : '',
-                style: TextStyle(
-                  color: !server.online ? theme.colorScheme.error : null,
-                ),
-              ),
-              const SizedBox(height: 15.0),
-              Transform.scale(
-                scale: 0.9,
-                child: OutlinedButton(
-                  child: Text(loc.disconnectServer),
-                  onPressed: () {
-                    onRemoveServer(context, server);
-                  },
-                ),
-              ),
-            ]),
-          ),
-          PositionedDirectional(
-            top: 4,
-            end: 2,
-            child: PopupMenuButton<Object>(
-              iconSize: 20.0,
-              splashRadius: 16.0,
-              position: PopupMenuPosition.under,
-              offset: const Offset(-128, 4.0),
-              constraints: const BoxConstraints(maxWidth: 180.0),
-              tooltip: loc.serverOptions,
-              itemBuilder: (context) {
-                return [
-                  PopupMenuItem(
-                    child: Text(loc.editServerInfo),
-                    onTap: () {
-                      WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-                        if (context.mounted) showEditServer(context, server);
-                      });
-                    },
-                  ),
-                  PopupMenuItem(
-                    child: Text(loc.disconnectServer),
-                    onTap: () {
-                      WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-                        if (context.mounted) {
+    void showMenu() {
+      final box = context.findRenderObject() as RenderBox;
+      final pos = box.localToGlobal(const Offset(6.0, 50.0));
+
+      showServerMenu(
+        context: context,
+        onRemoveServer: onRemoveServer,
+        server: server,
+        pos: pos,
+      );
+    }
+
+    return GestureDetector(
+      onSecondaryTap: showMenu,
+      child: SizedBox(
+        height: 180,
+        width: 180,
+        child: Card(
+          child: Stack(children: [
+            Positioned.fill(
+              bottom: 8.0,
+              left: 8.0,
+              right: 8.0,
+              top: 8.0,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.transparent,
+                      foregroundColor: theme.iconTheme.color,
+                      child: const Icon(Icons.dns, size: 30.0),
+                    ),
+                    const SizedBox(height: 8.0),
+                    Text(
+                      server.name,
+                      overflow: TextOverflow.ellipsis,
+                      style: theme.textTheme.titleSmall,
+                    ),
+                    Text(
+                      !isLoading
+                          ? [
+                              if (server.name != server.ip) server.ip,
+                            ].join()
+                          : loc.gettingDevices,
+                      overflow: TextOverflow.ellipsis,
+                      style: theme.textTheme.bodySmall,
+                    ),
+                    Text(
+                      !server.online
+                          ? loc.offline
+                          : !isLoading
+                              ? loc.nDevices(server.devices.length)
+                              : '',
+                      style: TextStyle(
+                        color: !server.online ? theme.colorScheme.error : null,
+                      ),
+                    ),
+                    const SizedBox(height: 15.0),
+                    Transform.scale(
+                      scale: 0.9,
+                      child: OutlinedButton(
+                        child: Text(loc.disconnectServer),
+                        onPressed: () {
                           onRemoveServer(context, server);
-                        }
-                      });
-                    },
-                  ),
-                  const PopupMenuDivider(height: 1.0),
-                  PopupMenuItem(
-                    child: Text(loc.browseEvents),
-                    onTap: () {
-                      home.setTab(UnityTab.eventsScreen.index, context);
-                    },
-                  ),
-                  PopupMenuItem(
-                    child: Text(loc.configureServer),
-                    onTap: () {
-                      launchUrl(Uri.parse(server.ip));
-                    },
-                  ),
-                  const PopupMenuDivider(height: 1.0),
-                  PopupMenuItem(
-                    child: Text(loc.refreshDevices),
-                    onTap: () async {
-                      servers.refreshDevices([server.id]);
-                    },
-                  ),
-                ];
-              },
+                        },
+                      ),
+                    ),
+                  ]),
             ),
-          ),
-          if (isLoading)
-            const PositionedDirectional(
-              start: 10,
-              top: 12,
-              child: SizedBox(
-                height: 18.0,
-                width: 18.0,
-                child: CircularProgressIndicator.adaptive(strokeWidth: 1.5),
+            PositionedDirectional(
+              top: 4,
+              end: 2,
+              child: IconButton(
+                iconSize: 20.0,
+                splashRadius: 16.0,
+                tooltip: loc.serverOptions,
+                icon: Icon(moreIconData),
+                onPressed: showMenu,
               ),
             ),
-        ]),
+            if (isLoading)
+              const PositionedDirectional(
+                start: 10,
+                top: 12,
+                child: SizedBox(
+                  height: 18.0,
+                  width: 18.0,
+                  child: CircularProgressIndicator.adaptive(strokeWidth: 1.5),
+                ),
+              ),
+          ]),
+        ),
       ),
     );
   }
+}
+
+Future showServerMenu({
+  required BuildContext context,
+  required Offset pos,
+  required Server server,
+  required OnRemoveServer onRemoveServer,
+}) {
+  final home = context.read<HomeProvider>();
+  final servers = context.read<ServersProvider>();
+  final loc = AppLocalizations.of(context);
+
+  return showMenu(
+    context: context,
+    position: RelativeRect.fromLTRB(
+      pos.dx,
+      pos.dy,
+      pos.dx + 180.0,
+      pos.dy + 200,
+    ),
+    color: Theme.of(context).dialogBackgroundColor,
+    items: <PopupMenuEntry>[
+      PopupMenuItem(
+        child: Text(loc.editServerInfo),
+        onTap: () {
+          WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+            if (context.mounted) showEditServer(context, server);
+          });
+        },
+      ),
+      PopupMenuItem(
+        child: Text(loc.disconnectServer),
+        onTap: () {
+          WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+            if (context.mounted) {
+              onRemoveServer(context, server);
+            }
+          });
+        },
+      ),
+      const PopupMenuDivider(height: 1.0),
+      PopupMenuItem(
+        child: Text(loc.browseEvents),
+        onTap: () {
+          home.setTab(UnityTab.eventsScreen.index, context);
+        },
+      ),
+      PopupMenuItem(
+        child: Text(loc.configureServer),
+        onTap: () {
+          launchUrl(Uri.parse(server.ip));
+        },
+      ),
+      const PopupMenuDivider(height: 1.0),
+      PopupMenuItem(
+        child: Text(
+          server.online ? loc.refreshDevices : loc.refreshServer,
+        ),
+        onTap: () async {
+          servers.refreshDevices([server.id]);
+        },
+      ),
+    ],
+  );
 }

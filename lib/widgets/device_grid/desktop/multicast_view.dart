f1d0eb34db5c9c3f11c681c2d85e9b41d594f348
Bruno D'Luka <brunodlukaa@gmail.com>
2023-11-09 11:06:53 -0300

    feat: switch between zoomed areas with the mouse wheel

--- lib/widgets/device_grid/desktop/multicast_view.dart
+++ lib/widgets/device_grid/desktop/multicast_view.dart
@@ -17,14 +17,18 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+import 'dart:async';
+import 'dart:math';
+
 import 'package:bluecherry_client/models/device.dart';
 import 'package:bluecherry_client/providers/settings_provider.dart';
 import 'package:bluecherry_client/widgets/misc.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:unity_video_player/unity_video_player.dart';
 
-class MulticastViewport extends StatelessWidget {
+class MulticastViewport extends StatefulWidget {
   final String? overlayText;
   final TextStyle? overlayStyle;
   final Offset? overlayPosition;
@@ -43,6 +47,55 @@ class MulticastViewport extends StatelessWidget {
   });
 
   @override
+  State<MulticastViewport> createState() => _MulticastViewportState();
+}
+
+class _MulticastViewportState extends State<MulticastViewport> {
+  Timer? _gap;
+
+  (int row, int column)? currentZoom;
+
+  /// Returns the next zoom position.
+  ///
+  /// [size] is the size of the matrix. For example, 4x4 matrix has a size of 4.
+  (int row, int column) nextZoom((int row, int column) current, int size) {
+    var row = current.$1;
+    var column = current.$2;
+
+    if (row == size - 1 && column == size - 1) {
+      row = 0;
+      column = 0;
+    } else if (column == size - 1) {
+      row++;
+      column = 0;
+    } else {
+      column++;
+    }
+
+    return (row, column);
+  }
+
+  /// Returns the previous zoom position.
+  ///
+  /// [size] is the size of the matrix. For example, 4x4 matrix has a size of 4.
+  (int row, int column) previousZoom((int row, int column) current, int size) {
+    var row = current.$1;
+    var column = current.$2;
+
+    if (row == 0 && column == 0) {
+      row = size - 1;
+      column = size - 1;
+    } else if (column == 0) {
+      row--;
+      column = size - 1;
+    } else {
+      column--;
+    }
+
+    return (row, column);
+  }
+
+  @override
   Widget build(BuildContext context) {
     final settings = context.watch<SettingsProvider>();
     final view = UnityVideoView.maybeOf(context);
@@ -51,26 +104,56 @@ class MulticastViewport extends StatelessWidget {
       return const SizedBox.shrink();
     }
 
+    final size = widget.device.matrixType.size;
     if (view.player.isCropped) {
-      return GestureDetector(
-        behavior: HitTestBehavior.opaque,
-        onTap: view.player.resetCrop,
-        child: const SizedBox.expand(),
+      return Listener(
+        onPointerSignal: (event) async {
+          if (_gap != null && _gap!.isActive || currentZoom == null) return;
+
+          if (event is PointerScrollEvent) {
+            if (event.scrollDelta.dy == 0.0) return;
+
+            final scaleChange = exp(event.scrollDelta.dy / 200).toInt();
+            _gap = Timer(const Duration(milliseconds: 1100), () {
+              _gap?.cancel();
+              _gap = null;
+            });
+
+            (int row, int column) next;
+
+            if (scaleChange == 1.0) {
+              next = nextZoom(currentZoom!, size);
+            } else {
+              next = previousZoom(currentZoom!, size);
+            }
+
+            view.player.crop(next.$1, next.$2, size);
+            debugPrint('next: $next');
+            currentZoom = next;
+          }
+        },
+        child: GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: () {
+            view.player.resetCrop();
+            currentZoom = null;
+          },
+          child: const SizedBox.expand(),
+        ),
       );
     }
 
-    final size = device.matrixType.size;
     return Stack(children: [
-      if (overlayText != null)
+      if (widget.overlayText != null)
         Positioned(
-          left: overlayPosition?.dx,
-          top: overlayPosition?.dy,
+          left: widget.overlayPosition?.dx,
+          top: widget.overlayPosition?.dy,
           child: IgnorePointer(
             child: Text(
-              overlayText!,
+              widget.overlayText!,
               style: TextStyle(
                 shadows: outlinedText(),
-              ).merge(overlayStyle),
+              ).merge(widget.overlayStyle),
             ),
           ),
         ),
@@ -88,6 +171,7 @@ class MulticastViewport extends StatelessWidget {
                 behavior: HitTestBehavior.opaque,
                 onTap: () {
                   view.player.crop(row, col, size);
+                  currentZoom = (row, col);
                 },
                 child: const SizedBox.expand(
                   child: IgnorePointer(child: Placeholder()),

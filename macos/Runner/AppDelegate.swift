01a562b6fa54314a8b33125bcc8007b5b50cf92e
Bruno D'Luka <brunodlukaa@gmail.com>
2023-12-06 17:28:50 -0300

    feat: do not terminal app after window is closed
    this ensures the app will properly dispose the player resources

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -4,6 +4,7 @@ import FlutterMacOS
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
+    // https://pub.dev/packages/window_manager#macos
+    return false
   }
 }

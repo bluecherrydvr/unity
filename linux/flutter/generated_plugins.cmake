7263c47211046fd0685778bb9bf0312055599efe
Bruno D'Luka <45696119+bdlukaa@users.noreply.github.com>
2023-12-11 10:37:57 -0300

    chore: downgrade app_links

--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -4,7 +4,6 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   awesome_notifications
-  gtk
   media_kit_libs_linux
   media_kit_video
   screen_retriever
--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/user249455/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/user249455/Documents/GitHub/unity"
+export "FLUTTER_ROOT=/home/bruno/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/bruno/Documents/paid_projects/fork/unity"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=3.0.014"
 export "FLUTTER_BUILD_NUMBER=3.0.014"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
-export "TREE_SHAKE_ICONS=true"
-export "PACKAGE_CONFIG=/Users/user249455/Documents/GitHub/unity/.dart_tool/package_config.json"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"

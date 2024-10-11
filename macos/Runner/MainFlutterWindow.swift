06d670bed2685180f9331b6de66faa0373324859
Bruno D'Luka <brunodlukaa@gmail.com>
2024-10-11 08:33:19 -0300

    feat: Add macos support

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -1,5 +1,8 @@
+// This file was modified according to https://pub.dev/packages/launch_at_startup#macos-support
+
 import Cocoa
 import FlutterMacOS
+import LaunchAtLogin // launch_at_startup
 
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
@@ -7,6 +10,26 @@ class MainFlutterWindow: NSWindow {
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
+
+    // <launch_at_startup>
+    // Add FlutterMethodChannel platform code
+    FlutterMethodChannel(
+      name: "launch_at_startup", binaryMessenger: flutterViewController.engine.binaryMessenger
+    )
+    .setMethodCallHandler { (_ call: FlutterMethodCall, result: @escaping FlutterResult) in
+      switch call.method {
+      case "launchAtStartupIsEnabled":
+        result(LaunchAtLogin.isEnabled)
+      case "launchAtStartupSetEnabled":
+        if let arguments = call.arguments as? [String: Any] {
+          LaunchAtLogin.isEnabled = arguments["setEnabledValue"] as! Bool
+        }
+        result(nil)
+      default:
+        result(FlutterMethodNotImplemented)
+      }
+    }
+    // </launch_at_startup>
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 

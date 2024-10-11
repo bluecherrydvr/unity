d6e544d1c3d9d60337b1be4e88c0d8686617f0d7
Bruno D'Luka <brunodlukaa@gmail.com>
2024-10-11 09:12:30 -0300

    chore: Recreate macos app

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/user249455/Documents/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/user249455/Documents/GitHub/unity"
+export "FLUTTER_ROOT=C:\Users\bruno\Documents\flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\bruno\Documents\flutter\paid_projects\unity"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/user249455/Documents/GitHub/unity/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=3.0.019"
 export "FLUTTER_BUILD_NUMBER=3.0.019"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/user249455/Documents/GitHub/unity/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -4,6 +4,7 @@ import FlutterMacOS
 @main
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    // This was changed according to:
     // https://pub.dev/packages/window_manager#macos
     return false
   }

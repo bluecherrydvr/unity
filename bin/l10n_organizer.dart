ce2781f492f8c1e4d7de77fdf416349343f9e554
Bruno D'Luka <brunodlukaa@gmail.com>
2025-03-09 13:37:34 -0300

    fix: Placeholders types

--- bin/l10n_organizer.dart
+++ bin/l10n_organizer.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:path/path.dart' as path;
 
 void main() {
   final files = Directory('${Directory.current.path}/lib/l10n')
@@ -15,10 +16,14 @@ void main() {
 
     final content = file.readAsStringSync();
     final contentMap = Map<String, dynamic>.from(json.decode(content));
+    contentMap
+        .removeWhere((key, value) => key.replaceAll('"', '').startsWith('@'));
 
     final newContentMap = <String, dynamic>{
       for (final key in mirrorMap.keys) key: contentMap[key] ?? mirrorMap[key],
     };
+    newContentMap['@@locale'] =
+        path.basenameWithoutExtension(file.path).split('_').last;
 
     final newContent =
         const JsonEncoder.withIndent('  ').convert(newContentMap);

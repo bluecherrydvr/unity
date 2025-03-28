df20901dfae03c6bdf5556cf54b5f13f34b080f2
Bruno D'Luka <brunodlukaa@gmail.com>
2025-03-28 15:14:46 -0300

    fix: Log errors to specific camera files

--- lib/utils/video_player.dart
+++ lib/utils/video_player.dart
@@ -148,9 +148,13 @@ class UnityPlayers with ChangeNotifier {
 
     setSource();
 
-    controller.onError.listen((event) {
+    controller.onError.listen((error) {
       writeLogToFile(
-        'An error ocurred when playing a video (${controller.dataSource}): $event\n',
+        'An error ocurred when playing a video (${controller.dataSource}): $error\n',
+      );
+      logStreamToFile(
+        device.url ?? '${device.name} (${device.server.ip})',
+        error,
       );
     });
 
@@ -187,9 +191,15 @@ class UnityPlayers with ChangeNotifier {
           ..setVolume(1.0)
           ..setSpeed(1.0);
 
-    controller.onError.listen((event) {
+    controller.onError.listen((error) {
       writeLogToFile(
-        'An error ocurred when playing a video (${controller.dataSource}): $event\n',
+        'An error ocurred when playing an event video (${controller.dataSource}): $error\n',
+      );
+      logStreamToFile(
+        event.mediaURL == null
+            ? 'Event ${event.title} (${event.id})'
+            : event.mediaPath,
+        error,
       );
     });
 
